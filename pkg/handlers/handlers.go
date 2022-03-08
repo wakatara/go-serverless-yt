8d82e8aa367f59fce0325cf86468aaa80a8358b4
Daryl Manning <daryl@wakatara.com>
2022-03-09 00:32:20 +0800

    Handler file and all user verbs sorted.

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -37,17 +37,38 @@ func GetUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dyn
 
 func CreatetUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (
 	*events.APIGatewayProxyResponse, error) {
-
+	
+	result, err := user.CreateUser(req, tableName, dynaclient)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error())
+		})
+	}
+	return apiResponse(http.StatusCreated, result)
 }
 
 func UpdateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (
 	*events.APIGatewayProxyResponse, error) {
 
+	result, err := user.UpdateUser(req, tableName, dynaclient)
+	if err != nil {
+		return apiRepsonse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error())
+		})
+	}
+	return apiResponse(http.StatusOK, result)
 }
 
 func DeleteUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (
 	*events.APIGatewayProxyResponse, error) {
 
+	err := user.DeletetUser(req, tableName, dynaClient)
+	if err != nil {
+		return apiRepsonse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error())
+		})
+	}
+	return apiResponse(http.StatusOK, nil)
 }
 
 func UnhandledMethod(*events.APIGatewayProxyResponse, error) {
